1024f4b934e15d4eb46ecee37ec8262b2bf14b51
iamnaheed <naheednawaz444@gmail.com>
2026-03-04 15:50:47 +0500

    Update head_pose_estimation.py

--- utils/head_pose_estimation.py
+++ utils/head_pose_estimation.py
@@ -1,1 +1,12 @@
 
+import math
+
+def calculate_head_angle(landmarks):
+    nose = landmarks[30]
+    chin = landmarks[8]
+
+    dx = nose[0] - chin[0]
+    dy = nose[1] - chin[1]
+
+    angle = math.degrees(math.atan2(dy, dx))
+    return abs(angle)
